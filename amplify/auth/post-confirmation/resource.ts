e735b054b5cd93bea06be846037d816cf9747c5f
slimzorz <blueleighw@gmail.com>
2025-06-23 12:59:08 -0400

    fixed and addded resoure.ts in data

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -1,26 +1,5 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-import { postConfirmation } from "../auth/post-confirmation/resource";
+import { defineFunction } from '@aws-amplify/backend';
 
-const schema = a
-  .schema({
-    UserProfile: a
-      .model({
-        email: a.string(),
-        profileOwner: a.string(),
-      })
-      .authorization((allow) => [
-        allow.ownerDefinedIn("profileOwner"),
-      ]),
-  })
-  .authorization((allow) => [allow.resource(postConfirmation)]);
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
-  },
+export const postConfirmation = defineFunction({
+  name: 'post-confirmation',
 });
